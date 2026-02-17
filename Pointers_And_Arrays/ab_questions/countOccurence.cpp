593b40922bb3eefe5e337a49cf7e5a9925786a26
Vishal <vishalkiranpatel@gmail.com>
2026-02-17 18:48:43 +0530

    count occurences

---
+++ Pointers_And_Arrays/ab_questions/countOccurence.cpp
@@ -0,0 +1,5 @@
+// WAP to count occurence of an element in an array
+// WAP to count occurence of a character in a string
+// WAP to count occurence of an element at even indices
+// WAP to count occurence of even numbers at even indices
+// if you can think of more variants, then WAP for them too
