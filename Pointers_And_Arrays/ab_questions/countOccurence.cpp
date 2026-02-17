d2790f331f3fa47cc5136114a0e7386f9203915f
Vishal <vishalkiranpatel@gmail.com>
2026-02-17 19:03:36 +0530

    questions on searching and basic array operations

--- Pointers_And_Arrays/ab_questions/countOccurence.cpp
+++ Pointers_And_Arrays/ab_questions/countOccurence.cpp
@@ -2,4 +2,4 @@
 // WAP to count occurence of a character in a string
 // WAP to count occurence of an element at even indices
 // WAP to count occurence of even numbers at even indices
-// if you can think of more variants, then WAP for them too
+// if you can think of more variants, then WAP for them too 
